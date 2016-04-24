63d9bcb2744b709938874e561b54c3434baaaef3
Shantanu Raj <s@sraj.me>
2016-04-25 04:06:20 +0530

    Add mail client info

--- config/mail.js
+++ config/mail.js
@@ -4,12 +4,17 @@
 
 'use strict'
 
+const nodemailer = require('nodemailer')
+const mg         = require('nodemailer-mailgun-transport')
+
 const API_KEY = 'key-5c1fc3a3b8cd274540898f16a1e062af'
 const domain  = 'sandbox4473c9277ff3462fa2fadd575a7ea138.mailgun.org'
 
-module.exports = {
+const auth = {
   auth: {
     api_key: API_KEY,
     domain: domain,
   }
 }
+
+module.exports = nodemailer.createTransport(mg(auth))
