dd4b59289f193e7777a9fd1885ccab2b8c968ddd
Shantanu Raj <s@sraj.me>
2016-04-25 03:43:52 +0530

    Add user schema

--- src/model.js
+++ src/model.js
@@ -4,3 +4,25 @@
 
 'use strict';
 
+const db = require('../config/db')
+
+exports.UserEntity = class UserEntity extends db.Model {
+  get tableName() {
+    return 'users';
+  }
+}
+
+exports.User = class User {
+    constructor(options) {
+        this.firstName  = options.firstName
+        this.lastName   = options.lastName
+        this.email      = options.email
+        this.password   = options.password
+        this.scheme     = options.scheme
+        this.red        = options.red
+        this.pink       = options.pink
+        this.orange     = options.orange
+        this.yellow     = options.yellow
+        this.status     = options.status
+    }
+}
