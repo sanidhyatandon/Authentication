7afa06f69a0c10d5b0bb2eadf8f8e56ac3c95ed0
Shantanu Raj <s@sraj.me>
2016-04-25 04:04:54 +0530

    Add mail config

---
+++ config/mail.js
@@ -0,0 +1,15 @@
+/**
+ * Mail configuration
+ */
+
+'use strict'
+
+const API_KEY = 'key-5c1fc3a3b8cd274540898f16a1e062af'
+const domain  = 'sandbox4473c9277ff3462fa2fadd575a7ea138.mailgun.org'
+
+module.exports = {
+  auth: {
+    api_key: API_KEY,
+    domain: domain,
+  }
+}
