9f75c0c1b6f50d612390ce32222370393f8f0cc1
Shantanu Raj <s@sraj.me>
2016-04-25 06:21:11 +0530

    Bind templates

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const Hapi   = require('hapi')
 const Inert  = require('inert')
+const Vision = require('vision')
+const Path   = require('path')
 const routes = require('./routes')
+const Handlebars = require('handlebars')
 
 const server = new Hapi.Server()
 
@@ -8,10 +11,14 @@ server.connection({
 	port: '8000'
 })
 
-server.register(Inert)
-
-server.route(routes)
-
-server.start()
+server.register([Inert, Vision])
+    .then(() => {
+        server.views({
+          engines: { html: Handlebars },
+          path: Path.join(__dirname, '..', 'public')
+        })
+    })
+    .then(() => server.route(routes))
+    .then(() => server.start())
 	.then(console.log(`Server running on ${server.info.uri}`))
 	.catch(console.log)
